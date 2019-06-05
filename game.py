c78cdfd046b2fa4278c3ee2c741b8ca76debc1db
jsr512 <50751736+jsr512@users.noreply.github.com>
2019-06-05 18:56:22 -0400

    Capture Inputs

--- game.py
+++ game.py
@@ -1,1 +1,20 @@
+#game.py
+
 print("Rock, Paper, Scissors, Shoot!")  
+
+#capture input
+
+user_choice = input("Please chose one of the following options: 'rock', 'paper' or 'scissors' (without the quotes):")
+
+print("User chose:",user_choice)
+
+
+#validate input
+
+#generate computer selection
+
+#determing the winner
+
+#display final outputs/outcomes 
+
+
