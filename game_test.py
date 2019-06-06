dc4140c44c5b214b19abb761aec355aaa85773a6
jsr512 <50751736+jsr512@users.noreply.github.com>
2019-06-05 20:49:22 -0400

    Testing Set Up

--- game_test.py
+++ game_test.py
@@ -1,4 +1,30 @@
-# game_test.py
+## game_test.py
+#
+#def test_example():
+#    assert 2 == 2 
 
-def test_example():
-    print("Testing")
+from game import my_message
+
+def test_my_message():
+    x = my_message()
+    assert x == "Hello"
+
+
+
+#    # game_test.py
+#
+#from game import determine_winner
+#
+#def test_determination_of_the_winner():
+#    assert determine_winner("rock", "rock") == None # represents a tie
+#    assert determine_winner("rock", "paper") == "paper"
+#    assert determine_winner("rock", "scissors") == "rock"
+#
+#    assert determine_winner("paper", "rock") == "paper"
+#    assert determine_winner("paper", "paper") == None # represents a tie
+#    assert determine_winner("paper", "scissors") == "scissors"
+#
+#    assert determine_winner("scissors", "rock") == "rock"
+#    assert determine_winner("scissors", "paper") == "scissors"
+#    assert determine_winner("scissors", "scissors") == None # represents a tie
+#
